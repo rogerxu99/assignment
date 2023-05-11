e35175c9f7cab16df39c5967ba856b355cff1445
rogerxu99 <102379945+rogerxu99@users.noreply.github.com>
2023-05-11 19:30:52 +0800

    Add files via upload

--- test/model/ImageModel.class.php
+++ test/model/ImageModel.class.php
@@ -1,1 +1,73 @@
+<?php 
 
+
+class ImageModel extends SQLite3
+{
+
+	private $id;
+	private $title;
+	private $imgSrc;
+	private $description;
+	private $createTime;
+
+
+    // public function __construct($id, $title, $imgSrc, $description, $createTime)
+    // {
+    // 	$this->id=$id;
+    // 	$this->title=$title;
+    // 	$this->imgSrc=$imgSrc;
+    // 	$this->description=$description;
+    // 	$this->createTime=$createTime;
+    // 	$this->showInfo();
+    // }
+
+    public function __construct()
+    {
+        $this->open('../db/test.db');
+    }
+
+
+
+    public function __destruct()
+    {
+    	self::close();
+    }
+
+
+    public function fetchAll($type)
+    {
+        $my_array = array();
+    	$sql = 'select * from t_img_info where type = '.$type;
+        $ret = self::query($sql);
+        while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+           array_push($my_array, $row);
+        }
+        // echo "Operation done successfully\n";
+        return $my_array;
+    }
+
+    public function fetchByName($name)
+    {
+        $my_array = array();
+        $sql = 'select * from t_img_info where type = 3 and headline = '.'"'.$name.'"';
+        $ret = self::query($sql);
+        while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+           array_push($my_array, $row);
+        }
+        // echo "Operation done successfully\n";
+        return $my_array;
+    }
+
+    public function getPathById($id)
+    {
+        $sql = 'select img_src as imgSrc from t_img_info where id = '.$id;
+        $ret = self::query($sql);
+        while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+            return $row['imgSrc'];
+        }
+        return '';
+    }
+
+}
+
+?>
