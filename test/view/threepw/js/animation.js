d4e6ca5ce1c3cd65e9e8cc6bd54fd5dbaa4be39a
rogerxu99 <102379945+rogerxu99@users.noreply.github.com>
2023-05-11 20:12:12 +0800

    Add files via upload

---
+++ test/view/threepw/js/animation.js
@@ -0,0 +1,98 @@
+let y = 1
+let rotateYInterval = null
+let rotateDom = document.querySelector('#rotation')
+
+
+function rotateY(){
+    if(rotateYInterval){
+        clearInterval(rotateYInterval)
+        rotateYInterval = null
+    } else {
+        rotateYInterval = setInterval(()=>{
+            y+=0.01
+            rotateDom.setAttribute('rotation',`0,1,0,${y}`)
+        },5)
+    }
+}
+
+
+let z = 1
+let rotateZInterval = null
+function rotateZ(){
+    if(rotateZInterval){
+        clearInterval(rotateZInterval)
+        rotateZInterval = null
+    } else {
+        rotateZInterval = setInterval(()=>{
+            z+=0.01
+            rotateDom.setAttribute('rotation',`0,0,1,${z}`)
+        },5)
+    }
+}
+
+let x = 1
+let rotateXInterval = null
+function rotateX(){
+    if(rotateXInterval){
+        clearInterval(rotateXInterval)
+        rotateXInterval = null
+    } else {
+        rotateXInterval = setInterval(()=>{
+            x+=0.01
+            rotateDom.setAttribute('rotation',`1,0,0,${x}`)
+        },5)
+    }
+}
+
+function stopAnimation(){
+    if(rotateXInterval){
+        clearInterval(rotateXInterval)
+        rotateXInterval = null
+    }
+    if(rotateZInterval){
+        clearInterval(rotateZInterval)
+        rotateZInterval = null
+    }
+    if(rotateYInterval){
+        clearInterval(rotateYInterval)
+        rotateYInterval = null
+    }
+}
+
+
+
+
+let headLightDom = document.querySelector('#head')
+let onOrOff = true
+function headLightOnOff(){
+    if(onOrOff){
+        headLightDom.setAttribute('headlight','false')
+        onOrOff = false
+    }else {
+        headLightDom.setAttribute('headlight','true')
+        onOrOff = true
+    }
+}
+function lightDefault(){
+    headLightDom.setAttribute('headlight','true')
+    onOrOff = true
+}
+
+function left(){
+    rotateDom.setAttribute('rotation',`0,1,0,1.5`)
+}
+
+function defaultO(){
+    rotateDom.setAttribute('rotation',`0,1,0,0`)
+
+}
+
+function right(){
+    rotateDom.setAttribute('rotation',`0,1,0,-1.5`)
+
+}
+
+
+function toGitHub(){
+    window.open('https://github.com/rogerxu99/Coke','blank')
+}
--- test/view/threepw/js/index.js
+++ test/view/threepw/js/index.js
@@ -1,1 +1,26 @@
 
+window.onload = function() {
+  $.get('https://users.sussex.ac.uk/~zx84/3dapp/assignment/test/controller/ImageControllerApi.php?type=1',(data,status)=>{
+        console.log(data)
+        data.forEach(e=>{
+            $('.right').append(`
+                <div class="item">
+                <div class="bg" style="background: url('https://users.sussex.ac.uk/~zx84/3dapp/assignment/test/controller/ImageView.php?id=${e.id}');background-size: 100% 100%"></div>
+                <div class="item-text">
+                    <h2>${e.headline}</h2>
+                    <h2>${e.subheading}</h2>
+                    <p style="width: 100%">
+                    ${e.description}
+               </p>
+                </div>
+            </div>
+            
+            `)
+        })
+
+    })
+}
+
+function toGitHub(){
+    window.open('https://github.com/rogerxu99/Coke','blank')
+}
